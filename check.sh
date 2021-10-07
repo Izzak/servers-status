0c6e663ee29af9421edf93ef857b21e38e4a462f
Tomáš Dočkal <tomas-dockal@seznam.cz>
2021-10-07 19:12:22 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -3,12 +3,11 @@ mkdir -p logs
 IFS='|' read -ra ODKAZY <<< "$1"
 for ODKAZ in "${ODKAZY[@]}"; do
     IFS='=' read -ra odkaz <<< "$ODKAZ"
+    echo "${odkaz[1]}"
     for i in 1 2 3 4 5; 
     do
         obsah=$(echo -n $(date '+%Y-%m-%d %H:%M:%S')", ";curl --write-out '%{response_code}, %{time_total}' --silent --max-time 10 --insecure --output /dev/null ${odkaz[1]})
-        #echo "$obsah";
         IFS="," read -a obsahArray <<< $obsah
-        echo "-${obsahArray[1]}-";
         if [ "${obsahArray[1]}" == " 200" ] || [ "${obsahArray[1]}" == " 202" ] || [ "${obsahArray[1]}" == " 301" ] || [ "${obsahArray[1]}" == " 307" ]; then
             result=" success,"
             break
@@ -17,6 +16,6 @@ for ODKAZ in "${ODKAZY[@]}"; do
             sleep 5
         fi
     done
-    echo "${obsah/${obsahArray[1]},/$result}"
+    echo "${obsah/${obsahArray[1]},/$result}" >> logs/${odkaz[0]}.log
 done
 find logs/ -iname '*.log' > logs/files.cfg
